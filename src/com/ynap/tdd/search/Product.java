1f593d7b329774508ce519c970bc6915a4d6aab7
Chris Young - External <christopher.young.external@ynap.com>
2018-07-05 15:15:44 +0100

    matchesWhenProductContainsMatchingAttribute - passes

--- src/com/ynap/tdd/search/Product.java
+++ src/com/ynap/tdd/search/Product.java
@@ -1,9 +1,19 @@
 package com.ynap.tdd.search;
 
 public class Product {
-
+	
+	Attribute attribute;
+	
 	public boolean matches(SearchCriterea searchCriterea) {
-		return false;
+		if(attribute == null) {
+			return false;
+		} else {
+			return attribute.getFacet().equals(searchCriterea.getAttribute().getFacet());
+		}
+	}
+
+	public void setAttribute(Attribute attribute) {
+		this.attribute = attribute;
 	}
 
 }
